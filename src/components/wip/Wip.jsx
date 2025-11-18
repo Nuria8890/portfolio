1f5cd1fe9fc79397eb8acfe92f8974213f7b965c
Nuria Campo <nuria.campo7@gmail.com>
2025-11-18 11:21:57 +0100

    add 'drawing' phone and computer

--- src/components/wip/Wip.jsx
+++ src/components/wip/Wip.jsx
@@ -3,13 +3,24 @@ import "@/styles/layout/Wip.scss";
 function wip() {
   return (
     <>
-      <main className="section">
-        <article className="section__mobile">
-          <div className="section__mobile__line-top"></div>
-          <h1 className="section__mobile__text">
+      <main className="wip">
+        <article className="wip__mobile">
+          <div className="wip__mobile__line-top"></div>
+          <h1 className="wip__mobile__text">
             Work <br /> In <br /> Progress
           </h1>
-          <div className="section__mobile__line-bottom"></div>
+          <div className="wip__mobile__line-bottom"></div>
+        </article>
+
+        <article className="wip__desktop">
+          <div className="wip__desktop__screen">
+            <div className="wip__desktop__screen__line-around">
+              <h1 className="wip__desktop__screen__text">
+                Work <br /> In <br /> Progress
+              </h1>
+            </div>
+          </div>
+          <div className="wip__desktop__line-bottom"></div>
         </article>
       </main>
       <footer className="footerWip">
