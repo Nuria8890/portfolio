b861de005221332bbb3932202f1771b78cea5596
Nuria Campo <nuria.campo7@gmail.com>
2025-11-19 13:43:04 +0100

    change the folder footer component and add new icon to tab

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,14 @@
 import "@/styles/App.scss";
 import Header from "@/components/header/Header";
-import Wip from "@/components/wip/Wip";
-import Footer from "@/components/wip/Footer";
+import Footer from "@/components/footer/Footer";
 
 function App() {
   return (
-    <div className="screen">
-      {/* <Header /> */}
-      <main>
-        <Wip />
-      </main>
+    <>
+      <Header />
+      <main></main>
       <Footer />
-    </div>
+    </>
   );
 }
 
