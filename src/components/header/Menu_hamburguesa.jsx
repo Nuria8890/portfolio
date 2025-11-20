1f8b1e74cf1c254a0564e32da0ec406caa71403d
Nuria Campo <nuria.campo7@gmail.com>
2025-11-20 13:24:14 +0100

    add openCloseMenu variable

--- src/components/header/Menu_hamburguesa.jsx
+++ src/components/header/Menu_hamburguesa.jsx
@@ -1,13 +1,14 @@
-import { CgMenuRound } from "react-icons/cg";
 import "@/styles/layout/header/Menu_hamburguesa.scss";
+import { CgMenuRound } from "react-icons/cg";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 
 function Menu_hamburguesa({ toggleMenu, menuIsOpen }) {
+  const openCloseMenu = menuIsOpen ? "menu__open" : "menu__close";
   return (
     <>
       <CgMenuRound className="menu__icon__open" onClick={toggleMenu} />
 
-      <nav className={`${menuIsOpen ? "menu__open" : "menu__close"}`}>
+      <nav className={openCloseMenu}>
         <IoIosCloseCircleOutline
           className="menu__icon__close"
           onClick={toggleMenu}
