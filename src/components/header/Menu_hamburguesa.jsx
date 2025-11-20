177631c757f71aa7901aed7b3c8a28241f6dbf2e
Nuria Campo <nuria.campo7@gmail.com>
2025-11-20 12:14:25 +0100

    close icon in menu hamburguesa

--- src/components/header/Menu_hamburguesa.jsx
+++ src/components/header/Menu_hamburguesa.jsx
@@ -1,13 +1,18 @@
 import { CgMenuRound } from "react-icons/cg";
 import "@/styles/layout/header/Menu_hamburguesa.scss";
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 function Menu_hamburguesa({ toggleMenu, menuIsOpen }) {
   return (
     <>
-      <CgMenuRound className="menu__icon" onClick={toggleMenu} />
+      <CgMenuRound className="menu__icon__open" onClick={toggleMenu} />
 
-      <nav className={`${menuIsOpen ? "menu__open" : "menu__close"}`}>
-        <li className="header__nav__list">
+      <nav className={`menu ${menuIsOpen ? "menu__open" : "menu__close"}`}>
+        <IoIosCloseCircleOutline
+          className="menu__icon__close"
+          onClick={toggleMenu}
+        />
+        <li>
           <ul>Sobre mi</ul>
           <ul>Proyectos</ul>
           <ul>Contacta</ul>
