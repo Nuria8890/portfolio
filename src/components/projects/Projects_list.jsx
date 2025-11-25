4de6d8c8d7c619968b13b03b4b0f9f894ada9eeb
Nuria Campo <nuria.campo7@gmail.com>
2025-11-25 12:31:44 +0100

    render projects separated by technology

--- src/components/projects/Projects_list.jsx
+++ src/components/projects/Projects_list.jsx
@@ -1,13 +1,14 @@
 import "@/styles/layout/projects/Projects_list.scss";
 import Project_card from "./Project_card";
 
-function Projects_list({ objetProjects }) {
-  const projectsArray = Object.values(objetProjects);
+function Projects_list({ arrayProjects }) {
+  console.log("arrayProjects es: ", arrayProjects);
 
-  const projectsElement = projectsArray.map((project, i) => {
+  const projectsToRender = arrayProjects.map((project, i) => {
     return <Project_card key={i} projectToRender={project} />;
   });
-  return <ul className="projects__container">{projectsElement}</ul>;
+
+  return <ul className="projects__container">{projectsToRender}</ul>;
 }
 
 export default Projects_list;
