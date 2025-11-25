51889119813bde329e3894fcf2374aeac69c13e1
Nuria Campo <nuria.campo7@gmail.com>
2025-11-25 13:05:30 +0100

    add filter options

--- src/components/projects/filters/FilterByTech.jsx
+++ src/components/projects/filters/FilterByTech.jsx
@@ -1,5 +1,13 @@
 function FilterByTech() {
-  return <div>filtro por tecnología</div>;
+  return (
+    <select>
+      <option value="">Selecciona una tecnología</option>
+      <option value="html">HTML y CSS</option>
+      <option value="js">JavaScript vanilla</option>
+      <option value="react">React</option>
+      <option value="db">Base de datos</option>
+    </select>
+  );
 }
 
 export default FilterByTech;
