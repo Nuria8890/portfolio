ac3dcbc87eccb4a6df58af1a67721a2cee98fd30
Nuria Campo <nuria.campo7@gmail.com>
2025-11-25 10:58:40 +0100

    add technology used in each project, in the data object

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,6 +3,13 @@ import Projects_list from "./Projects_list";
 function projects({ objetProjects }) {
   return (
     <section>
+      <p>Proyectos de HTM y CSS</p>
+      <Projects_list objetProjects={objetProjects} />
+      <p>Proyectos con JavaScript vanilla</p>
+      <Projects_list objetProjects={objetProjects} />
+      <p>Proyectos con Frameworks</p>
+      <Projects_list objetProjects={objetProjects} />
+      <p>Proyectos utilizando DB</p>
       <Projects_list objetProjects={objetProjects} />
     </section>
   );
