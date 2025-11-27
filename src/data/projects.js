1f904c1640d9470ffdd3efc8fd999c9097fe2d5c
Nuria Campo <nuria.campo7@gmail.com>
2025-11-27 09:25:11 +0100

    add titles and texts to data projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,58 +10,74 @@ import PhotoA4Ev from "@/assets/a4Ev.jpeg";
 
 const adalabModulo1Grupal = {
   url: "https://nuria8890.github.io/ADALAB-project-module-1-team-2-TechAnimal/",
-  text: "Es una página web realizada con HTML, SCSS y Vite, tiene un diseño responsive mobile first utilizando mediaqueries, por lo que la web se adapta al tamaño de pantalla de distintos dispositivos, y utilizamos control de versiones con Git y GitHub, con el que aprendimos a mejorar la comunicación entre los miembros del equipo.Para el despliegue de este proyecto hemos utilizado GitHub pages.",
   photo: PhotoA1Gru,
   tech: "html",
-  date: "dsgdsa",
+  title: "Tech Animal",
+  text: "Es una página web realizada con HTML, SCSS y Vite, tiene un diseño responsive mobile first utilizando mediaqueries, por lo que la web se adapta al tamaño de pantalla de distintos dispositivos, y utilizamos control de versiones con Git y GitHub, con el que aprendimos a mejorar la comunicación entre los miembros del equipo. Para el despliegue de este proyecto hemos utilizado GitHub pages.",
 };
 
 const adalabModulo1Evaluacion = {
   url: "https://nuria8890.github.io/ADALAB-modulo-1-evaluacion-final-Nuria8890/",
   photo: PhotoA1Ev,
   tech: "html",
+  title: "Comienzos compartidos",
+  text: "Es una página web realizada con HTML, SCSS y Vite, tiene un diseño responsive mobile first utilizando mediaqueries, por lo que la web se adapta al tamaño de pantalla de distintos dispositivos, y utilicé control de versiones con Git y GitHub, con el que mejoré la organización de las tareas. Para el despliegue de este proyecto he utilizado GitHub pages.",
 };
 
 const adalabModulo2Grupal = {
   url: "https://nuria8890.github.io/ADALAB-project-module-2-team-3-HastaElInfinito/",
   photo: PhotoA2Gru,
   tech: "js",
+  title: "Hasta el infinito - Web grupal",
+  text: "Es una página web realizada con HTML, SCSS, JavaScript vanilla y Vite, donde el usuario puede crear tarjetas personalizadas con temática ToyStory. A destacar: - uso de variables, estructuras de datos, condicionales y funciones, - manipulación del DOM y respuesta a eventos del usuario, - manejo de estructuras de datos complejas, con arrays y objetos, - peticiones al servidor (fetch), - almacenaje de datos en local (localStorage). Para el despliegue de este proyecto hemos utilizado GitHub pages.",
 };
 
 const adalabModulo2EvaluacionIntermedia = {
   url: "https://nuria8890.github.io/ADALAB-modulo-2-evaluacion-intermedia-Nuria8890/",
   photo: PhotoA2Int,
   tech: "js",
+  title: "Adivina el número",
+  text: "Es una web sencilla, realizada con HTML, CSS y JS. Es un juego donde el usuario intenta adivinar un número aleatorio. El código está bien organizado en funciones específicas, lo que facilita su comprensión y mantenimiento. He utilizado: - 'strict mode' para evitar errores comunes y mejorar la seguridad del código, - métodos de JS para crear un número aleatorio, - selectores de clase para interactuar con el DOM, - condicionales y funciones manejadoras para manejar eventos. Disfrútalo! 👾 🎮",
 };
 
 const adalabModulo2Evaluacion = {
   url: "https://nuria8890.github.io/ADALAB-modulo-2-evaluacion-final-Nuria8890/",
   photo: PhotoA2Ev,
   tech: "js",
+  title: "Buscador de series de anime",
+  text: "Es una web realizada con HTML, CSS y JS vanilla, donde el usuario puede buscar y guardar sus series de anime favoritas. El código está bien organizado en funciones específicas, lo que facilita su legibilidad y mantenimiento. A destacar: - uso de API, - localStorage para guardar las preferencias de usuario, - actualización del DOM de forma eficiente y dinámica, - manejo de eventos efectivo, añadiendo escuchas a varios elementos (buscar, reset, eliminar de favoritos) Disfrútalo!",
 };
 
 const adalabModulo3EvaluacionIntermedia = {
   url: "https://nuria8890.github.io/ADALAB-modulo-3-evaluacion-intermedia-Nuria8890/",
   photo: PhotoA3Int,
   tech: "react",
+  title: "Listado de países",
+  text: "Es una web realizada con React, donde el usuario puede buscar países por nombre y continente. El código está bien organizado en componentes, lo que ayuda a que sea claro, fácil de mantener y funcional. A destacar: - uso de JSON con el contenido de los países - useState para manejar el estado y renderizar los componentes automáticamente - utilización de una lógica de filtrado flexible, que permite combinar múltiples criterios de forma efectiva Descubre los países de cada continente!",
 };
 
 const adalabModulo3Evaluacion = {
   url: "https://nuria8890.github.io/ADALAB-modulo-3-evaluacion-final-Nuria8890/",
   photo: PhotoA3Ev,
   tech: "react",
+  title: "Rick and Morty characters",
+  text: "Es una web realizada con React, donde el usuario puede filtrar personajes de la serie Rick&Morty por nombre, especie y estado. El código está bien organizado en componentes, lo que ayuda a que sea claro, fácil de mantener y funcional. A destacar: - API de rickandmortyapi.com - uso adecuado de useState y useEffect, que permite manejar el estado y los efectos secundarios de manera efectiva - almacenamiento de datos en localStorage Wubba lubba dub dub!",
 };
 
 const adalabModulo3y4Grupal = {
   url: "https://promo-a-module-4-team-2-proyectos.onrender.com/",
   photo: PhotoA3y4Gru,
   tech: "db",
+  title: "4Code - Web grupal",
+  text: "Es una página web realizada con React, donde el usuario puede introducir los datos de un proyecto y obtener una vista maquetada con esa información. A destacar: - migración de código a React - creación de servidor con NodeJS y ExpressJS - creación de API Rest - creación de bbdd con MySQL - peticiones al servidor (fetch) - envío de parámetros al API Rest (query, body, url, header) - respuesta del servidor (endpoints) Para el despliegue de este proyecto hemos utilizado la plataforma Render.",
 };
 
 const adalabModulo4Evaluacion = {
   url: "https://nuria8890.github.io/ADALAB-modulo-4-evaluacion-final-bpw-Nuria8890/",
   photo: PhotoA4Ev,
   tech: "db",
+  title: "Roler db",
+  text: "He creado un servidor con NodeJS y Express que gestiona una API para una tienda ficticia de patines. El código está bien estructurado, con funciones separadas para el manejo de rutas y operaciones, lo que facilita la lectura y el mantenimiento. A destacar: - endpoints (POST, GET, PUT, DELETE) - implementación de validación básica para asegurar que los datos requeridos están presentes antes de realizar operaciones en la bbdd - manejo de errores comunes devolviendo respuestas adecuadas al cliente",
 };
 
 /* TODO: 
