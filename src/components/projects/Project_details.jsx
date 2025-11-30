59c8321423e9563178d88753212ed07d06b9f58f
Nuria Campo <nuria.campo7@gmail.com>
2025-11-30 11:27:51 +0100

    styles to project details

--- src/components/projects/Project_details.jsx
+++ src/components/projects/Project_details.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router";
+import "@/styles/layout/projects/Project_details.scss";
 
 function Project_details({ arrayProjects }) {
   const { idProject } = useParams();
@@ -8,20 +9,23 @@ function Project_details({ arrayProjects }) {
   });
 
   return (
-    <section>
-      <div>
-        <img src={selectedProject.photo} alt={selectedProject.title} />
-        <div>
-          <h3>{selectedProject.title}</h3>
-          <span>
-            {selectedProject.date.toLocaleDateString("es-ES", {
-              year: "numeric",
-              month: "long",
-            })}
-          </span>
+    <section className="detailsContainer">
+      <img
+        className="detailsContainer__img"
+        src={selectedProject.photo}
+        alt={selectedProject.title}
+      />
+      <div className="detailsContainer__text">
+        <h3>{selectedProject.title}</h3>
+        <span>
+          Proyecto realizado en{" "}
+          {selectedProject.date.toLocaleDateString("es-ES", {
+            year: "numeric",
+            month: "long",
+          })}
+        </span>
 
-          <p dangerouslySetInnerHTML={{ __html: selectedProject.longText }}></p>
-        </div>
+        <p dangerouslySetInnerHTML={{ __html: selectedProject.longText }}></p>
       </div>
     </section>
   );
